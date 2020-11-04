a6faa22ff7399228a08bfec618ff4271d72866b5
Niranjan2048 <68651509+Niranjan2048@users.noreply.github.com>
2020-11-04 15:39:28 +0530

    Add files via upload

---
+++ invent.py
@@ -0,0 +1,178 @@
+from tkinter import *
+from tkinter import ttk
+import pymysql
+from tkinter import messagebox
+class Customer:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("----INVENTORY MANAGEMENT----")
+        self.root.geometry("1600x800+0+0")
+        title=Label(self.root,text="INVENTORY MANAGEMENT",bd=10,relief=SOLID,fg="black",bg="cyan",font=("times new roman",40,"bold"))
+        title.pack(side=TOP,fill=X)
+        #-----------------variables---------
+        self.product_id=StringVar()
+        self.product_name=StringVar()
+        self.product_qty=IntVar()
+        self.search_by=StringVar()
+        self.search_txt=StringVar()
+
+
+        #----------MANAGE FRAME----------
+        
+        self.manage_frame=Frame(self.root,bd=5,relief=RIDGE,bg="ivory")
+        self.manage_frame.place(x=20,y=100,width=600,height=600)
+        m_title=Label(self.manage_frame,text="PRODUCT MANAGEMENT",bg="ivory",fg="black",font=("times new roman",20,"bold"))
+        m_title.grid(row=0,columnspan=2,pady=20)
+
+        lbl_id=Label(self.manage_frame,text="PRODUCT ID",bg="ivory",fg="black",font=("times new roman",16,"bold"))
+        lbl_id.grid(row=1,column=0,pady=10,padx=20,sticky="w")
+
+        self.txt_id=Entry(self.manage_frame,textvariable=self.product_id,font=("times new roman",16,"bold"),bd=5,relief=GROOVE)
+        self.txt_id.grid(row=1,column=1,pady=10,padx=20,sticky="w")
+        
+        lbl_name=Label(self.manage_frame,text="PRODUCT NAME",bg="ivory",fg="black",font=("times new roman",16,"bold"))
+        lbl_name.grid(row=2,column=0,pady=10,padx=20,sticky="w")
+
+        self.txt_name=Entry(self.manage_frame,textvariable=self.product_name,font=("times new roman",16,"bold"),bd=5,relief=GROOVE)
+        self.txt_name.grid(row=2,column=1,pady=10,padx=20,sticky="w")
+        
+        lbl_qty=Label(self.manage_frame,text=" QUANTITY",bg="ivory",fg="black",font=("times new roman",16,"bold"))
+        lbl_qty.grid(row=3,column=0,pady=10,padx=20,sticky="w")
+
+       
+
+        self.txt_qty=Entry(self.manage_frame,textvariable=self.product_qty,font=("times new roman",16,"bold"),bd=5,relief=GROOVE)
+        self.txt_qty.grid(row=3,column=1,pady=10,padx=20,sticky="w")
+        
+
+         #----------BUTTON FRAME----------
+        self.btn_frame=Frame(self.manage_frame,bd=5,relief=GROOVE,bg="ivory")
+        self.btn_frame.place(x=20,y=450,width=420)
+
+        add_btn=Button(self.btn_frame,text="ADD",width=10,command=self.add_product).grid(row=0,column=0,padx=10,pady=10)
+        update_btn=Button(self.btn_frame,text="UPDATE",width=10,command=self.update_data).grid(row=0,column=1,padx=10,pady=10)
+        delete_btn=Button(self.btn_frame,text="DeLETE",width=10,command=self.delete_data).grid(row=0,column=2,padx=10,pady=10)
+        clear_btn=Button(self.btn_frame,text="CLEAR",width=10,command=self.clear).grid(row=0,column=3,padx=10,pady=10)
+        
+
+
+
+
+
+
+        #----------DETAILS FRAME----------
+        self.details_frame=Frame(self.root,bd=5,relief=RIDGE,bg="ivory")
+        self.details_frame.place(x=640,y=100,width=840,height=600)
+        
+        lbl_search=Label(self.details_frame,text="SEARCH BY",bg="ivory",fg="black",font=("times new roman",16,"bold"))
+        lbl_search.grid(row=0,column=0,pady=10,padx=20,sticky="w")
+
+        combo_search=ttk.Combobox(self.details_frame,textvariable=self.search_by,font=("times new roman",13),state='readonly',width=10)
+        combo_search['values']=("product_name","product_id")
+        combo_search.grid(row=0,column=1,padx=20,pady=10)
+
+        self.txt_search=Entry(self.details_frame,width=15,textvariable=self.search_txt,font=("times new roman",16,"bold"),bd=5,relief=GROOVE)
+        self.txt_search.grid(row=0,column=2,pady=10,padx=20,sticky="w")
+        
+        search_btn=Button(self.details_frame,text="SEARCH",width=10,command=self.search_data).grid(row=0,column=3,padx=10,pady=10)
+        showall_btn=Button(self.details_frame,text="SHOW ALL",width=10,command=self.fetch_data).grid(row=0,column=4,padx=10,pady=10)
+
+        #-----------table frame---------
+        self.table_frame=Frame(self.details_frame,bd=4,relief=RIDGE,bg="ivory")
+        self.table_frame.place(x=10,y=70,width=760,height=490)
+        
+        scroll_x=Scrollbar(self.table_frame,orient=HORIZONTAL)
+        scroll_y=Scrollbar(self.table_frame,orient=VERTICAL)
+        self.invent_table=ttk.Treeview(self.table_frame,columns=("PRODUCT ID","PRODUCT NAME","QUANTITY"),xscrollcommand=scroll_x.set,yscrollcommand=scroll_y.set)
+        scroll_x.pack(side=BOTTOM,fill=X)
+        scroll_y.pack(side=RIGHT,fill=Y)
+        scroll_x.config(command=self.invent_table.xview)
+        scroll_y.config(command=self.invent_table.yview)
+        self.invent_table.heading("PRODUCT ID",text="PRODUCT ID:")
+        self.invent_table.heading("PRODUCT NAME",text="PRODUCT NAME:")
+        self.invent_table.heading("QUANTITY",text="QUANTITY:")
+        self.invent_table['show']='headings'
+        self.invent_table.column("PRODUCT ID",width=100)
+        self.invent_table.column("PRODUCT NAME",width=250)
+        self.invent_table.column("QUANTITY",width=100)
+
+        self.invent_table.pack(fill=BOTH,expand=1)
+        self.invent_table.bind("<ButtonRelease-1>",self.get_cursor)
+        
+        self.fetch_data()
+
+    def add_product(self):
+        if self.product_id.get()=="" or self.product_name.get()=="":
+            messagebox.showerror("Error","All fields arer required!!")
+        else:
+            con=pymysql.connect(host="localhost",user="root",password="",database="php")
+            cur=con.cursor()
+            cur.execute("insert into inventory values(%s,%s,%s)",(self.product_id.get(),self.product_name.get(),self.product_qty.get()))
+            con.commit()
+            self.fetch_data()
+            self.clear()
+            con.close()
+            messagebox.showinfo("success","record has been inserte")
+
+    def fetch_data(self): 
+        con=pymysql.connect(host="localhost",user="root",password="",database="php")
+        cur=con.cursor()
+        cur.execute("select * from inventory")
+        rows=cur.fetchall()
+        if len(rows)!=0:
+            self.invent_table.delete(*self.invent_table.get_children())
+            for row in rows:
+                self.invent_table.insert('',END,values=row)
+                con.commit()
+        con.close()
+    def clear(self):
+        self.product_id.set("")
+        self.product_name.set("")
+        self.product_qty.set("")  
+
+    def get_cursor(self,ev):
+        cursor_row=self.invent_table.focus()
+        content=self.invent_table.item(cursor_row)
+        row=content['values']
+        self.product_id.set(row[0])
+        self.product_name.set(row[1])
+        self.product_qty.set(row[2])  
+    def update_data(self):
+        con=pymysql.connect(host="localhost",user="root",password="",database="php")
+        cur=con.cursor()
+        cur.execute("update inventory set product_name=%s,product_qty=%s where product_id=%s",(self.product_name.get(),self.product_qty.get(),self.product_id.get()))
+        con.commit()
+        self.fetch_data()
+        self.clear()
+        con.close()
+    def delete_data(self):
+        con=pymysql.connect(host="localhost",user="root",password="",database="php")
+        cur=con.cursor()
+        cur.execute("delete  from inventory where product_id=%s",self.product_id.get())
+        con.commit()
+        con.close()
+        self.fetch_data()
+        self.clear()
+    def search_data(self): 
+        con=pymysql.connect(host="localhost",user="root",password="",database="php")
+        cur=con.cursor()
+        cur.execute("select * from inventory where"+str(self.search_by.get())+"LIKE '%"+str(self.search_txt.get())+"%'")
+        rows=cur.fetchall()
+        if len(rows)!=0:
+            self.invent_table.delete(*self.invent_table.get_children())
+            for row in rows:
+                self.invent_table.insert('',END,values=row)
+                con.commit()    
+        
+
+                      
+        
+
+
+
+
+
+
+root=Tk() 
+obj=Customer(root)
+root.mainloop()
