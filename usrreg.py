0e5c058f1fc73ee4a7ab2a1e9fa83a73ac9207f8
MufcVarun11 <73822176+MufcVarun11@users.noreply.github.com>
2020-12-13 14:21:52 +0530

    Varun Nair

--- usrreg.py
+++ usrreg.py
@@ -3,14 +3,27 @@ from tkinter import messagebox
 from tkinter import ttk
 from PIL import ImageTk, Image
 import mysql.connector
+
 class User_Register:
     def __init__(self,root):
         self.root = root
         self.root.title('Inventory Management')
         self.root.geometry("1600x800")
-        self.root.config(bg="White")
+        self.root.iconbitmap('C:/Users/User pc/Documents/Mini Project/button images/Iconinv.ico')
 
+                      ##DESIGN#######  
+
+        self.bg= ImageTk.PhotoImage(Image.open("C:/Users/User pc/Documents/Mini Project/button images/og bacground.jpg"))
+        bg= Label(self.root,image=self.bg)
+        bg.place(x=0,y=0,relwidth=1,relheight=1)
+        self.login_b=ImageTk.PhotoImage(Image.open("C:/Users/User pc/Documents/Mini Project/button images/button_login (2).png"))
+        log_btn=Label(self.root,image=self.login_b)
+        log_btn.place(x=175, y=550, height = 68, width = 250)
+        self.regs_b=ImageTk.PhotoImage(Image.open("C:/Users/User pc/Documents/Mini Project/button images/button_register.png"))
+        #reg_btn=Label(self.root,image=self.reg_btn)
+        #reg_btn.place(x=175, y=400, height = 75, width = 300)
         def back():
+
             self.root.destroy()
             import usrlogin
 
@@ -52,36 +65,36 @@ class User_Register:
                 import usrlogin                                     
 
 
-        self.usrn_frame = LabelFrame(self.root, text ="SIGN UP", padx = 10, pady = 10, font =("Arial",20))
-        self.usrn_frame.place(x=550, y=125, height = 550, width = 700)
-        usrnew_1st = Label(self.usrn_frame, text = "First Name     : ",font =("Arial",16) ).grid(row = 1, column = 0, padx = 10, pady = 10)
+        self.usrn_frame = LabelFrame(self.root, text ="SIGN UP", padx = 10, pady = 10, font =("Arial",20),bg="Orange")
+        self.usrn_frame.place(x=650, y=125, height = 550, width = 700)
+        usrnew_1st = Label(self.usrn_frame, text = "First Name     : ",font =("Arial",16),bg="light goldenrod").grid(row = 1, column = 0, padx = 10, pady = 10)
         self.usrnew_1sten = Entry(self.usrn_frame, width = 40, font =("Arial",16))
         self.usrnew_1sten.grid(row = 1, column = 1, padx = 10, pady = 10)
-        usrnew_last = Label(self.usrn_frame, text = "Last Name     : ",font =("Arial",16)).grid(row = 2, column = 0, padx = 10, pady = 10)
+        usrnew_last = Label(self.usrn_frame, text = "Last Name     : ",font =("Arial",16),bg="light goldenrod").grid(row = 2, column = 0, padx = 10, pady = 10)
         self.usrnew_lasten = Entry(self.usrn_frame, width = 40, font =("Arial",16))
         self.usrnew_lasten.grid(row = 2, column = 1, padx = 10, pady = 10)
-        usrnew_id = Label(self.usrn_frame, text = "E-mail id        : ", font =("Arial",16)).grid(row = 3, column = 0, padx = 10, pady = 10)
+        usrnew_id = Label(self.usrn_frame, text = "E-mail id        : ", font =("Arial",16),bg="light goldenrod").grid(row = 3, column = 0, padx = 10, pady = 10)
         self.usrnew_iden = Entry(self.usrn_frame, width = 40, font =("Arial",16))
         self.usrnew_iden.grid(row = 3, column = 1, padx = 10, pady = 10)
-        usrnew_pass = Label(self.usrn_frame, text = "Password      : ", font =("Arial",16)).grid(row = 6, column = 0, padx = 10, pady = 10)
+        usrnew_pass = Label(self.usrn_frame, text = "Password      : ", font =("Arial",16),bg="light goldenrod").grid(row = 6, column = 0, padx = 10, pady = 10)
         self.usrnew_passen = Entry(self.usrn_frame,show = "*", width = 40, font =("Arial",16))
         self.usrnew_passen.grid(row = 6, column = 1, padx = 10, pady = 10)
-        security = Label(self.usrn_frame, text = "Security Que : ", font =("Arial",16)).grid(row = 4, column = 0, padx = 10, pady = 10)
+        security = Label(self.usrn_frame, text = "Security Que : ", font =("Arial",16),bg="light goldenrod").grid(row = 4, column = 0, padx = 10, pady = 10)
         self.security_combo = ttk.Combobox(self.usrn_frame, font =("Arial",16), state = "readonly" , width = 39, justify = "center")
         self.security_combo['values']=("Select","Your favourite book","Your favourite movie","Your best friend")
         self.security_combo.grid(row = 4, column = 1, padx = 10, pady = 10 )
         self.security_combo.current(0)
-        answer = Label(self.usrn_frame, text = "Answer         : ", font =("Arial",16)).grid(row = 5, column = 0, padx = 10, pady = 10)
+        answer = Label(self.usrn_frame, text = "Answer         : ", font =("Arial",16),bg="light goldenrod").grid(row = 5, column = 0, padx = 10, pady = 10)
         self.answeren = Entry(self.usrn_frame, width = 40, font =("Arial",16))
         self.answeren.grid(row = 5, column = 1, padx = 10, pady = 10)
-        usrnew_passcn = Label(self.usrn_frame, text = "Confirm Pass : ", font =("Arial",16)).grid(row = 7, column = 0, padx = 10, pady = 10)
+        usrnew_passcn = Label(self.usrn_frame, text = "Confirm Pass : ", font =("Arial",16),bg="light goldenrod").grid(row = 7, column = 0, padx = 10, pady = 10)
         self.usrnew_passcnen = Entry(self.usrn_frame,show = "*", width = 40, font =("Arial",16))
         self.usrnew_passcnen.grid(row = 7, column = 1, padx = 10, pady = 10)
-        self.usrreg_login = Button (self.usrn_frame, text = "REGISTER", command = unew_login, font = ("Arial",18) )
+        self.usrreg_login = Button (self.usrn_frame, image=self.regs_b, command = unew_login, font = ("Arial",18) )
         self.usrreg_login.place(x=175, y=400, height = 75, width = 300)
-        self.signup = Button(self.root, text = "LOGIN", command = login, font = ("Arial",18) )
-        self.signup.place(x=175, y=550, height = 65, width = 250)
-        self.back_btn = Button(self.root, text  = "<BACK", font =("Arial",18),bd="0",bg="White",fg="RED", command= back)
+        self.signup = Button(self.root, image=self.login_b, command = login, font = ("Arial",18) )
+        self.signup.place(x=175, y=550, height = 68, width = 250)
+        self.back_btn = Button(self.root, text  = "<BACK", font =("Arial",18),bd="0",bg="Light Blue",fg="RED", command= back)
         self.back_btn.grid(row=0, column=0)
 
 root = Tk()
