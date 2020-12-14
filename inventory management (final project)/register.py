d23974432a3f4fee1e2dae28ad202f964a6be8d6
Niranjan2048 <68651509+Niranjan2048@users.noreply.github.com>
2020-12-14 20:29:36 +0530

    final projecr

---
+++ inventory management (final project)/register.py
@@ -0,0 +1,102 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+from PIL import ImageTk, Image
+import mysql.connector
+
+class User_Register:
+    def __init__(self,root):
+        self.root = root
+        self.root.title('Inventory Management')
+        self.root.geometry("1600x800")
+        self.root.iconbitmap('C:/Users/Niranjan Khedkar/OneDrive/Desktop/Stock-Management-System-master/inventory/button images(login)/Iconinv.ico')
+
+                      ##DESIGN#######  
+
+        self.bg= ImageTk.PhotoImage(Image.open("C:/Users/Niranjan Khedkar/OneDrive/Desktop/Stock-Management-System-master/inventory/button images(login)/og bacground.jpg"))
+        bg= Label(self.root,image=self.bg)
+        bg.place(x=0,y=0,relwidth=1,relheight=1)
+        self.login_b=ImageTk.PhotoImage(Image.open("C:/Users/Niranjan Khedkar/OneDrive/Desktop/Stock-Management-System-master/inventory/button images(login)/button_login (2).png"))
+        log_btn=Label(self.root,image=self.login_b)
+        log_btn.place(x=175, y=550, height = 68, width = 250)
+        self.regs_b=ImageTk.PhotoImage(Image.open("C:/Users/Niranjan Khedkar/OneDrive/Desktop/Stock-Management-System-master/inventory/button images(login)/button_register.png"))
+        #reg_btn=Label(self.root,image=self.reg_btn)
+        #reg_btn.place(x=175, y=400, height = 75, width = 300)
+        def back():
+
+            self.root.destroy()
+            import startpage
+
+
+        def login():
+            self.root.destroy()
+            import startpage
+
+        def unew_login():
+            if self.usrnew_1sten.get() =="" or self.usrnew_lasten.get()=="" or self.usrnew_iden.get()=="" or self.answeren.get()=="" or self.usrnew_passen.get()=="" or self.usrnew_passcnen.get()=="":
+                messagebox.showerror("Error","All fields are mandatory",parent = self.root)
+            elif self.usrnew_passen.get() != self.usrnew_passcnen.get():
+                messagebox.showerror("Error","Password does not match",parent = self.root)
+            else:
+                try:
+                    con = mysql.connector.connect(host="localhost",user="root",passwd="",database="Inventory")
+                    cur = con.cursor()
+                    select = "SELECT mail_id FROM USERS"
+                    cur.execute(select)
+                    for (mail_id) in cur:
+                        if self.usrnew_iden.get() == mail_id:
+                            messagebox.showerror("Error","User already exists",parent = self.root)
+                        else:
+                            login_insert = "INSERT INTO users (f_name , l_name, mail_id , que , answer , password) VALUES (%s, %s, %s, %s, %s, %s)"
+                            keys = (self.usrnew_1sten.get(),
+                                    self.usrnew_lasten.get(),
+                                    self.usrnew_iden.get(),
+                                    self.security_combo.get(),
+                                    self.answeren.get(),
+                                    self.usrnew_passen.get()
+                                    )
+                            cur.execute(login_insert,keys)
+                            con.commit()
+                            con.close()
+                            messagebox.showinfo("Success","Registration successful",parent = self.root)
+                except Exception as es:
+                    messagebox.showerror("Error",f"Error due to {es}",parent = self.root)
+                self.root.destory()
+                import startpage                                     
+
+
+        self.usrn_frame = LabelFrame(self.root, text ="SIGN UP", padx = 10, pady = 10, font =("Arial",20),bg="Orange")
+        self.usrn_frame.place(x=650, y=125, height = 550, width = 700)
+        usrnew_1st = Label(self.usrn_frame, text = "First Name     : ",font =("Arial",16),bg="light goldenrod").grid(row = 1, column = 0, padx = 10, pady = 10)
+        self.usrnew_1sten = Entry(self.usrn_frame, width = 40, font =("Arial",16))
+        self.usrnew_1sten.grid(row = 1, column = 1, padx = 10, pady = 10)
+        usrnew_last = Label(self.usrn_frame, text = "Last Name     : ",font =("Arial",16),bg="light goldenrod").grid(row = 2, column = 0, padx = 10, pady = 10)
+        self.usrnew_lasten = Entry(self.usrn_frame, width = 40, font =("Arial",16))
+        self.usrnew_lasten.grid(row = 2, column = 1, padx = 10, pady = 10)
+        usrnew_id = Label(self.usrn_frame, text = "E-mail id        : ", font =("Arial",16),bg="light goldenrod").grid(row = 3, column = 0, padx = 10, pady = 10)
+        self.usrnew_iden = Entry(self.usrn_frame, width = 40, font =("Arial",16))
+        self.usrnew_iden.grid(row = 3, column = 1, padx = 10, pady = 10)
+        usrnew_pass = Label(self.usrn_frame, text = "Password      : ", font =("Arial",16),bg="light goldenrod").grid(row = 6, column = 0, padx = 10, pady = 10)
+        self.usrnew_passen = Entry(self.usrn_frame,show = "*", width = 40, font =("Arial",16))
+        self.usrnew_passen.grid(row = 6, column = 1, padx = 10, pady = 10)
+        security = Label(self.usrn_frame, text = "Security Que : ", font =("Arial",16),bg="light goldenrod").grid(row = 4, column = 0, padx = 10, pady = 10)
+        self.security_combo = ttk.Combobox(self.usrn_frame, font =("Arial",16), state = "readonly" , width = 39, justify = "center")
+        self.security_combo['values']=("Select","Your favourite book","Your favourite movie","Your best friend")
+        self.security_combo.grid(row = 4, column = 1, padx = 10, pady = 10 )
+        self.security_combo.current(0)
+        answer = Label(self.usrn_frame, text = "Answer         : ", font =("Arial",16),bg="light goldenrod").grid(row = 5, column = 0, padx = 10, pady = 10)
+        self.answeren = Entry(self.usrn_frame, width = 40, font =("Arial",16))
+        self.answeren.grid(row = 5, column = 1, padx = 10, pady = 10)
+        usrnew_passcn = Label(self.usrn_frame, text = "Confirm Pass : ", font =("Arial",16),bg="light goldenrod").grid(row = 7, column = 0, padx = 10, pady = 10)
+        self.usrnew_passcnen = Entry(self.usrn_frame,show = "*", width = 40, font =("Arial",16))
+        self.usrnew_passcnen.grid(row = 7, column = 1, padx = 10, pady = 10)
+        self.usrreg_login = Button (self.usrn_frame, image=self.regs_b, command = unew_login, font = ("Arial",18) )
+        self.usrreg_login.place(x=175, y=400, height = 75, width = 300)
+        self.signup = Button(self.root, image=self.login_b, command = login, font = ("Arial",18) )
+        self.signup.place(x=175, y=550, height = 68, width = 250)
+        self.back_btn = Button(self.root, text  = "<BACK", font =("Arial",18),bd="0",bg="Light Blue",fg="RED", command= back)
+        self.back_btn.grid(row=0, column=0)
+
+root = Tk()
+obj = User_Register(root)
+root.mainloop()
---
+++ inventory management (final project)/supplier.py
@@ -0,0 +1,224 @@
+from tkinter import *
+from tkinter import ttk
+import pymysql
+from tkinter import messagebox
+from PIL import Image,ImageTk
+import math,random
+
+
+
+class Customer:
+    def __init__(self,root,id):
+        self.root = root
+        self.root.title("----INVENTORY MANAGEMENT----")
+        self.root.geometry("1600x800+0+0")
+        bg_color="Royalblue3"
+        bg_titlecolor="navy"
+        title=Label(self.root,text="INVENTORY MANAGEMENT",bd=10,relief=SOLID,fg="WHITE",bg=bg_titlecolor,font=("times new roman",40,"bold"))
+        title.pack(side=TOP,fill=X)
+        
+        #-----------------variables---------
+        self.product_id=StringVar()
+        self.product_name=StringVar()
+        self.product_qty=IntVar()
+        self.search_by=StringVar()
+        self.search_txt=StringVar()
+        self.product_price=IntVar()
+        self.user_id = StringVar()
+        self.product_id.set(str(random.randint(1000,9999)))
+        
+
+       #----------MANAGE FRAME----------
+        
+        self.manage_frame=Frame(self.root,bd=5,relief=RIDGE,bg=bg_color)
+        self.manage_frame.place(x=20,y=100,width=600,height=600)
+        m_title=Label(self.manage_frame,text="PRODUCT MANAGEMENT",bg=bg_color,fg="black",font=("times new roman",20,"bold"))
+        m_title.grid(row=0,columnspan=2,pady=20)
+
+        lbl_id=Label(self.manage_frame,text="USER ID",bg=bg_color,fg="black",font=("times new roman",16,"bold"))
+        lbl_id.grid(row=1,column=0,pady=10,padx=20,sticky="w")
+
+        self.txt_id=Entry(self.manage_frame,textvariable=self.user_id,font=("times new roman",16,"bold"),bd=5,relief=GROOVE,state=DISABLED)
+        self.user_id.set(id)
+        self.txt_id.grid(row=1,column=1,pady=10,padx=20,sticky="w")
+
+        lbl_id=Label(self.manage_frame,text="PRODUCT ID",bg=bg_color,fg="black",font=("times new roman",16,"bold"))
+        lbl_id.grid(row=2,column=0,pady=10,padx=20,sticky="w")
+
+        self.txt_id=Entry(self.manage_frame,textvariable=self.product_id,font=("times new roman",16,"bold"),bd=5,relief=GROOVE)
+        self.txt_id.grid(row=2,column=1,pady=10,padx=20,sticky="w")
+        
+        lbl_name=Label(self.manage_frame,text="PRODUCT NAME",bg=bg_color,fg="black",font=("times new roman",16,"bold"))
+        lbl_name.grid(row=3,column=0,pady=10,padx=20,sticky="w")
+
+        self.txt_name=Entry(self.manage_frame,textvariable=self.product_name,font=("times new roman",16,"bold"),bd=5,relief=GROOVE)
+        self.txt_name.grid(row=3,column=1,pady=10,padx=20,sticky="w")
+        
+        lbl_qty=Label(self.manage_frame,text=" QUANTITY",bg=bg_color,fg="black",font=("times new roman",16,"bold"))
+        lbl_qty.grid(row=4,column=0,pady=10,padx=20,sticky="w")
+
+        self.txt_qty=Entry(self.manage_frame,textvariable=self.product_qty,font=("times new roman",16,"bold"),bd=5,relief=GROOVE)
+        self.txt_qty.grid(row=4,column=1,pady=10,padx=20,sticky="w")
+        
+        lbl_price=Label(self.manage_frame,text="PRICE (PER UNIT)",bg=bg_color,fg="black",font=("times new roman",16,"bold"))
+        lbl_price.grid(row=5,column=0,pady=10,padx=20,sticky="w")
+
+        self.txt_price=Entry(self.manage_frame,textvariable=self.product_price,font=("times new roman",16,"bold"),bd=5,relief=GROOVE)
+        self.txt_price.grid(row=5,column=1,pady=10,padx=20,sticky="w")
+
+
+        #-----------ALL IMAGES--------------
+        self.search_b1=ImageTk.PhotoImage(file="C:/Users/Niranjan Khedkar/OneDrive/Desktop/Stock-Management-System-master/inventory/button_images/search1.png")
+        self.add_b1=ImageTk.PhotoImage(file="C:/Users/Niranjan Khedkar/OneDrive/Desktop/Stock-Management-System-master/inventory/button_images/add2.png")
+        self.update_b1=ImageTk.PhotoImage(file="C:/Users/Niranjan Khedkar/OneDrive/Desktop/Stock-Management-System-master/inventory/button_images/update1.png")
+        self.delete_b1=ImageTk.PhotoImage(file="C:/Users/Niranjan Khedkar/OneDrive/Desktop/Stock-Management-System-master/inventory/button_images/remove.png")
+        self.clear_b1=ImageTk.PhotoImage(file="C:/Users/Niranjan Khedkar/OneDrive/Desktop/Stock-Management-System-master/inventory/button_images/clear2.png")
+        self.show_b1=ImageTk.PhotoImage(file="C:/Users/Niranjan Khedkar/OneDrive/Desktop/Stock-Management-System-master/inventory/button_images/show1.png")
+        
+        #----------BUTTON FRAME----------
+        self.btn_frame=Frame(self.manage_frame,bd=5,relief=GROOVE,bg=bg_color)
+        self.btn_frame.place(x=20,y=450,width=440)
+
+        add_btn=Button(self.btn_frame,image=self.add_b1,width=85,height=35,bg=bg_color,bd=0,command=self.add_product).grid(row=0,column=0,padx=10,pady=10)
+        update_btn=Button(self.btn_frame,image=self.update_b1,width=85,height=35,bg=bg_color,bd=0,command=self.update_data).grid(row=0,column=1,padx=10,pady=10)
+        delete_btn=Button(self.btn_frame,image=self.delete_b1,width=85,height=35,bg=bg_color,bd=0,command=self.delete_data).grid(row=0,column=2,padx=10,pady=10)
+        clear_btn=Button(self.btn_frame,image=self.clear_b1,width=85,height=35,bg=bg_color,bd=0,command=self.clear).grid(row=0,column=3,padx=10,pady=10)
+        
+          
+          
+         #----------DETAILS FRAME----------
+        self.details_frame=Frame(self.root,bd=5,relief=RIDGE,bg=bg_color)
+        self.details_frame.place(x=640,y=100,width=840,height=600)
+        
+        lbl_search=Label(self.details_frame,text="SEARCH BY",bg=bg_color,fg="black",font=("times new roman",16,"bold"))
+        lbl_search.grid(row=0,column=0,pady=10,padx=20,sticky="w")
+
+        combo_search=ttk.Combobox(self.details_frame,textvariable=self.search_by,font=("times new roman",13),state='readonly',width=10)
+        combo_search['values']=("product_name","product_id")
+        combo_search.grid(row=0,column=1,padx=20,pady=10)
+
+        self.txt_search=Entry(self.details_frame,width=15,textvariable=self.search_txt,font=("times new roman",16,"bold"),bd=5,relief=GROOVE)
+        self.txt_search.grid(row=0,column=2,pady=10,padx=20,sticky="w")
+        
+        search_btn=Button(self.details_frame,image=self.search_b1,text="SEARCH",width=90,height=30,bd=0,bg=bg_color,command=self.search_data).grid(row=0,column=3,padx=10,pady=10)
+        showall_btn=Button(self.details_frame,image=self.show_b1,width=90,height=30,bg=bg_color,bd=0,command=self.fetch_data).grid(row=0,column=4,padx=10,pady=10)
+
+        #-----------table frame---------
+        self.table_frame=Frame(self.details_frame,bd=4,relief=RIDGE,bg=bg_color)
+        self.table_frame.place(x=10,y=70,width=760,height=490)
+        
+        
+        scroll_x=Scrollbar(self.table_frame,orient=HORIZONTAL)
+        scroll_y=Scrollbar(self.table_frame,orient=VERTICAL)
+        self.invent_table=ttk.Treeview(self.table_frame,columns=("PRODUCT ID","PRODUCT NAME","QUANTITY","PRICE","TOTAL"),xscrollcommand=scroll_x.set,yscrollcommand=scroll_y.set)
+        scroll_x.pack(side=BOTTOM,fill=X)
+        scroll_y.pack(side=RIGHT,fill=Y)
+        scroll_x.config(command=self.invent_table.xview)
+        scroll_y.config(command=self.invent_table.yview)
+        self.invent_table.heading("PRODUCT ID",text="PRODUCT ID:")
+        self.invent_table.heading("PRODUCT NAME",text="PRODUCT NAME:")
+        self.invent_table.heading("QUANTITY",text="QUANTITY:")
+        self.invent_table.heading("PRICE",text="PRICE:")
+        self.invent_table.heading("TOTAL",text="TOTAL:")
+        self.invent_table['show']='headings'
+        self.invent_table.column("PRODUCT ID",width=70)
+        self.invent_table.column("PRODUCT NAME",width=200)
+        self.invent_table.column("QUANTITY",width=150)
+        self.invent_table.column("PRICE",width=150)
+        self.invent_table.column("TOTAL",width=150)
+
+        self.invent_table.pack(fill=BOTH,expand=1)
+        self.invent_table.bind("<ButtonRelease-1>",self.get_cursor)
+        
+        self.fetch_data()
+    
+    
+    def add_product(self):
+        if self.product_id.get()=="" or self.product_name.get()=="":
+            messagebox.showerror("Error","All fields arer required!!")
+        else:
+            self.total = float(self.product_qty.get())*float(self.product_price.get())
+
+        con=pymysql.connect(host="localhost",user="root",password="",database="inventory")
+        cur=con.cursor()
+        cur.execute("insert into inventory values(%s,%s,%s,%s,%s,%s)",(self.user_id.get(),self.product_id.get(),self.product_name.get(),self.product_qty.get(),self.product_price.get(),self.total))
+        con.commit()
+        self.fetch_data()
+        self.clear()
+        con.close()
+        messagebox.showinfo("success","record has been inserted",parent = self.root)
+
+    
+    def fetch_data(self): 
+        con=pymysql.connect(host="localhost",user="root",password="",database="inventory")
+        cur=con.cursor()
+        cur.execute("select product_id,product_name,product_qty,product_price,total from inventory")
+        rows=cur.fetchall()
+        if len(rows)!=0:
+            self.invent_table.delete(*self.invent_table.get_children())
+            for row in rows:
+                self.invent_table.insert('',END,values=row)
+                con.commit()
+        con.close()
+
+    
+    def clear(self):
+        self.product_id.set(str(random.randint(1000,9999)))
+        self.product_name.set("")
+        self.product_qty.set("")  
+        self.product_price.set("") 
+
+    
+    def get_cursor(self,ev):
+        cursor_row=self.invent_table.focus()
+        content=self.invent_table.item(cursor_row)
+        row=content['values']
+        self.product_id.set(row[0])
+        self.product_name.set(row[1])
+        self.product_qty.set(row[2])
+        self.product_price.set(row[3]) 
+        
+        
+    def update_data(self):
+        con=pymysql.connect(host="localhost",user="root",password="",database="inventory")
+        cur=con.cursor()
+        cur.execute("update inventory set product_name=%s,product_qty=%s where product_id=%s",(self.product_name.get(),self.product_qty.get(),self.product_id.get()))
+        con.commit()
+        self.fetch_data()
+        self.clear()
+        con.close()
+    
+    
+    def delete_data(self):
+        con=pymysql.connect(host="localhost",user="root",password="",database="inventory")
+        cur=con.cursor()
+        cur.execute("delete  from inventory where product_id=%s",self.product_id.get())
+        con.commit()
+        con.close()
+        self.fetch_data()
+        self.clear()
+    
+    
+    def search_data(self): 
+        con=pymysql.connect(host="localhost",user="root",password="",database="inventory")
+        cur=con.cursor()
+        if f'{self.search_by.get()}'=='product_id':
+            statement=f"select product_id,product_name,product_qty,product_price,total from inventory where product_id='{self.search_txt.get()}'"
+        else:
+            statement=f"select product_id,product_name,product_qty,product_price,total from inventory where product_name='{self.search_txt.get()}'"
+        cur.execute(statement)
+        self.invent_table.delete(*self.invent_table.get_children())
+        rows=cur.fetchall()
+        if len(rows)!=0:
+            self.invent_table.delete(*self.invent_table.get_children())
+            for row in rows:
+                self.invent_table.insert('',END, values=row)
+            con.commit()
+        con.close()
+
+
+def run_supplier(id):
+    root=Tk() 
+    obj=Customer(root,id)
+    print(id)
+    root.mainloop()
